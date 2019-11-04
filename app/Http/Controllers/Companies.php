1a6f5126e7a875a456b7463fa302a41fa604a73b
Benjin Thomas <Deepthi>
2019-11-04 20:26:15 +0200

    first commit

---
+++ app/Http/Controllers/Companies.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Companies extends Controller
+{
+    public function show() {
+        $employee=['bike','driving license'];
+       
+    }
+}
